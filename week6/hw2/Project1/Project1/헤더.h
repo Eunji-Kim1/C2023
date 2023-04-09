5ae6ef6f7f38b6c1801a94ad271e12a9ff2e3783
Eunji-Kim1 <126849481+Eunji-Kim1@users.noreply.github.com>
2023-04-09 23:04:44 +0900

    hw2

---
+++ week6/hw2/Project1/Project1/헤더.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+//파라미터가 있는 최대공약수 구하는 함수
+int max1(int a, int b);
+//파라미터가 없는 최대공약수 구하는 함수
+void max2(void);
+
+//파라미터가 있는 최소공배수 구하는 함수
+int min1(int a, int b);
+//파라미터가 없는 최소공배수 구하는 함수
+void min2(void);
+
+//파라미터가 있는 소수 판별함수
+int prime1(int num);
+//파라미터가 없는 소수 판별함수
+void prime2(void);
